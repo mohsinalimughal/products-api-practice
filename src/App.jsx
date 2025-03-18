b20f55c9629acdc53e80d946683704ddcb11d612
mohsinalimughal <mohsinalimughal112233445566@gmail.com>
2025-03-18 18:19:46 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useEffect, useState } from 'react'
+import  ButtonUsage  from './btn.jsx'
+import  ResponsiveAppBar  from './nav.jsx'
+import  ActionAreaCard  from './card.jsx'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products , setproducts] = useState([])
+  const [error , seterror] = useState(false)
+
+  useEffect(()=>{
+
+    fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then((res)=>{
+    setproducts(res.products)
+    console.log(res.products)
+  })
+  .catch(()=>{
+    seterror(true)
+  })
+
+
+
+  },[])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<ResponsiveAppBar />
+
+    <div      style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      gap: '1rem',
+      padding: '1rem',
+      margin: '1rem'
+    }}>
+
+       { products.map((product)=>{
+         return <ActionAreaCard title={product.title} image={product.thumbnail} description={product.description} />
+       })}
+
+    </div>
+
+
     </>
   )
 }
